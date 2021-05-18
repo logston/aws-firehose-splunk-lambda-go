1e3a3dbeaf286fffc7e0c2cba2c6b969e32d44b3
Paul Logston <paul.logston@gmail.com>
2021-05-18 01:56:50 -0400

    Add all major structs for message decoding

--- main.go
+++ main.go
@@ -1,17 +1,108 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
 	"context"
-	"fmt"
+	"encoding/base64"
+	"encoding/json"
+	"io/ioutil"
+
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
+type EventRecord struct {
+	RecordId                    string `json:"recordId"`
+	ApproximateArrivalTimestamp int    `json:"approximateArrivalTimestamp"`
+	Data                        string `json:"data"`
 }
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+type Event struct {
+	InvocationId      string        `json:"invocationId"`
+	DeliveryStreamArn string        `json:"deliveryStreamArn"`
+	Region            string        `json:"region"`
+	Records           []EventRecord `json:"records"`
+}
+
+type ResultRecord struct {
+	RecordId string `json:"recordId"`
+	Result   string `json:"result"`
+	Data     string `json:"data"`
+}
+
+type ResultResponse struct {
+	Records []ResultRecord `json:"records"`
+}
+
+type LogEvent struct {
+	Id        string `json:"id"`
+	Timestamp int    `json:"timestamp"`
+	Message   string `json:"message"`
+}
+
+type Message struct {
+	MessageType         string     `json:"messageType"`
+	Owner               string     `json:"owner"`
+	LogGroup            string     `json:"logGroup"`
+	LogStream           string     `json:"logStream"`
+	SubscriptionFilters []string   `json:"subscriptionFilters"`
+	LogEvents           []LogEvent `json:"logEvents"`
+}
+
+func transformMessage() {}
+
+func gunzip(b *bytes.Buffer, gzippedData []byte) error {
+	gr, err := gzip.NewReader(bytes.NewBuffer(gzippedData))
+	defer gr.Close()
+
+	data, err := ioutil.ReadAll(gr)
+	if err != nil {
+		return err
+	}
+
+	_, err = b.Write(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func HandleRequest(ctx context.Context, e Event) (ResultResponse, error) {
+	// Open the event
+	resultRecords := []ResultRecord{}
+
+	// For each record, transform the record.
+	for _, r := range e.Records {
+		gzippedData, err := base64.StdEncoding.DecodeString(r.Data)
+		if err != nil {
+			return ResultResponse{}, err
+		}
+
+		b := &bytes.Buffer{}
+		if err = gunzip(b, gzippedData); err != nil {
+			return ResultResponse{}, err
+		}
+
+		m := &Message{}
+		if err = json.Unmarshal(b.Bytes(), m); err != nil {
+			return ResultResponse{}, err
+		}
+
+		result := ResultRecord{
+			RecordId: r.RecordId,
+			Result:   "Ok",
+			Data:     m.LogEvents[0].Message,
+		}
+
+		resultRecords = append(resultRecords, result)
+	}
+
+	response := ResultResponse{
+		Records: resultRecords,
+	}
+
+	return response, nil
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHandleRequest(t *testing.T) {
+	ctx := context.Background()
+
+	eventRecords := []EventRecord{
+		{
+			RecordId:                    "1",
+			ApproximateArrivalTimestamp: 1621224132233,
+			Data:                        "H4sICIkuo2AAA2RhdGEtbWVzc2FnZS5qc29uAOWSz07DMAzG73uKqOetchznH7dqlB0AgbTdEELZFqpKXTu1HQhNe3eSlm0vwAWRQ2T5+2znZ+U4YSzZ+a5zhV997X1yw5LbbJW9PebLZbbIk2k0NJ+1b6PEUZBU2lgYhaopFm1z2A9lrncPTXERln3r3S4qvi5nbr3x2/dr/cxV1WjtDutu05b7vmzqu7LqfduFopcgBXFMzM69Q/L13D//8HV/tR6HO0jlNs4UikuSINEqBMONISIVJmtLxmhSWqHRoLUBAdxKSSTDwZFr6NOXYS+920U4rpAjEhgDABfHz97iOGRXNBZ5YQQGa7SSJJAzDinnKU9RnGNMORomAIRi1kpiikkmQVzGEcVQiBBazbL5PH9esaf7ZHjAafqL3PyfcuMf4o6ff3KafANy+R18swMAAA==",
+		},
+	}
+
+	e := Event{
+		InvocationId:      "not-used",
+		DeliveryStreamArn: "arn:aws:firehose:us-east-1:1234567890:deliverystream/DataLog",
+		Region:            "us-east-1",
+		Records:           eventRecords,
+	}
+
+	r, err := HandleRequest(ctx, e)
+	require.NoError(t, err)
+
+	require.Equal(t, nil, r)
+}
